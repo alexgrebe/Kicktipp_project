67eb12a3efcfef500a1bd14f2266e81186571941
Felix Farke <felix.farke@stud.uni-due.de>
2022-11-11 10:58:07 +0100

    Login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminRegisterComponent } from './admin-register/admin-register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    AdminRegisterComponent
+    AdminRegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
