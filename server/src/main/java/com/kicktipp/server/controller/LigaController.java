b5af2c8fdd5e264fa54954d9f3b94d90848ef07e
Felix Farke <felix.farke@stud.uni-due.de>
2022-11-12 11:54:09 +0100

    controller

--- server/src/main/java/com/kicktipp/server/controller/LigaController.java
+++ server/src/main/java/com/kicktipp/server/controller/LigaController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RestController
@@ -40,6 +42,11 @@ public class LigaController {
 
     @PostMapping("/readInCSV/{id}")
     public String csvController(@PathVariable Long id, @RequestBody MultipartFile file) {
+        try{
+        service.readCSV(new BufferedReader(new InputStreamReader(file.getInputStream())), id);
         return "success";
+        }
+        catch(Exception e) {
+        return e.getMessage();}
     }
 }
