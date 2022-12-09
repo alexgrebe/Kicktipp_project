77bae98cee6208f770b9516219dcf15c5687b7de
federfehnchen <felixfarke@icloud.com>
2022-12-09 16:30:27 +0100

    fix

--- server/src/main/java/com/kicktipp/server/controller/FreundController.java
+++ server/src/main/java/com/kicktipp/server/controller/FreundController.java
@@ -4,7 +4,6 @@ import com.kicktipp.server.model.Benutzer;
 import com.kicktipp.server.model.Freundschaftsanfragen;
 import com.kicktipp.server.service.AuthService;
 import com.kicktipp.server.service.FreundService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
