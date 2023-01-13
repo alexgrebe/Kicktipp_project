be7029d19ce1bcaff34618ab2bfc4d13bec4f7b5
federfehnchen <felixfarke@icloud.com>
2023-01-13 14:31:16 +0100

    wette erstellen

--- server/src/main/java/com/kicktipp/server/controller/WettenController.java
+++ server/src/main/java/com/kicktipp/server/controller/WettenController.java
@@ -1,10 +1,13 @@
 package com.kicktipp.server.controller;
 
+import com.kicktipp.server.model.Benutzer;
+import com.kicktipp.server.model.Wette;
 import com.kicktipp.server.service.AuthService;
 import com.kicktipp.server.service.WetteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:80", "http://localhost"}, allowCredentials = "true")
 @RestController
@@ -15,4 +18,16 @@ public class WettenController {
     @Autowired
     private WetteService wetteService;
 
+    @PostMapping("/createWette")
+    public ResponseEntity<String> createWette(@CookieValue(value = "auth_token", required = false)String token, @RequestBody Wette wette) {
+        try{
+        if(token == null || authService.verifyToken(token)) return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        Benutzer b = authService.getUserDetailsByToken(token);
+        wetteService.createWette(wette, b);
+        return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+        }
+        catch(Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- server/src/main/java/com/kicktipp/server/service/WetteService.java
+++ server/src/main/java/com/kicktipp/server/service/WetteService.java
@@ -1,5 +1,7 @@
 package com.kicktipp.server.service;
 
+import com.kicktipp.server.model.Benutzer;
+import com.kicktipp.server.model.Wette;
 import com.kicktipp.server.repository.WetteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,10 @@ public class WetteService {
     @Autowired
     private WetteRepository wetteRepository;
 
-    public void createWette() {}
+    public void createWette(Wette wette, Benutzer b) throws Exception {
+        if(b.getGeld()<wette.getEinsatz() || !b.isWetterlaubnis() ) throw new Exception("");
+        wetteRepository.save(wette);
+    }
 
     public void zulassungEntscheiden() {}
 
